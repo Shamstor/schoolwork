fc961d12433d6c9eb551f1793ed90232fdf8af9c
brettmccausland <brettmccausland@yahoo.com>
2017-09-02 19:06:40 -0700

    s

--- cs014/cs014/hw1/hw1.cpp
+++ cs014/cs014/hw1/hw1.cpp
@@ -11,7 +11,7 @@
 #include<cstdlib>
 #include<cmath>
 //hello
-
+//sssssfhfhfhfhfhfhffhfhfhhfhfhfhfhfhfhfhfhfhffhfhfgdgdgdhdhdhdhdhdhhfhfhfhfhfhfhfhfhfhfhfhfhhfhfhfhfhfhffhfhfh
 using namespace std;
 
 //returns true if testing vector is better than best case found //Test has less coins than best
